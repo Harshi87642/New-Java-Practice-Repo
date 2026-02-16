9ebf4069d746423f121d4abb16ab075f117b8f99
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-16 23:55:35 +0530

    Simple practice program 1

---
+++ rh01.java
@@ -0,0 +1,37 @@
+// Adding of two numbers Using methods
+
+
+class Calculator
+{
+  public int add(int n1, int n2)
+  {
+     return n1+n2;
+   }
+}
+
+public class rh01
+{
+  public static void main(String[] args)
+  {
+ 
+     Calculator  c = new Calculator();
+ 
+     int result = c.add(3,4);
+     System.out.println(Sum of two numbers: ",+result);
+  }
+}
+
+
+
+
+
+
+
+
+
+
+
+```````````````````````````````````````````````````````````
+
+OUTPUT:
+Sum of two numbers: 7
