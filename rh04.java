fc468098b0622410461b1e91bfa9bd8e4e44e177
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-16 23:59:31 +0530

    Simple practice program 4

---
+++ rh04.java
@@ -0,0 +1,36 @@
+// Array concept
+
+
+public class rh04
+{
+  public static void main(String[] args)
+  {
+
+      int num[] = new int[4];
+
+        num[0]=1;
+         num[1]=2;
+        num[2]=3;
+        num[3]=4;
+
+       for(int i=0;i<=num.length;i++)
+        {
+          System.out.println(i);
+         }
+     
+      System.out.println("Value at 1st position: "+num[1]);
+   }
+}
+
+
+
+
+
+``````````````````````````````````````````
+OUTPUT:
+1
+2
+3
+4
+Value at 1st position: 2
+      
