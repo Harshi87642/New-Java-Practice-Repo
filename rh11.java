3bed8d2a1bbe13512aa328f0e75d7c1489218321
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-17 23:37:17 +0530

    Simple practice program 11

---
+++ rh11.java
@@ -0,0 +1,28 @@
+// char at and concat operations
+
+
+public class rh11
+{
+  public static void mian(String[] args)
+  {
+ 
+    String str = "Hello";
+ 
+    System.out.println("Character at position 1 : "+str.charAt(1));
+
+    Ststem.out.println("After concatenation: " +str.concat("Java"));
+  }
+}
+
+
+
+
+
+
+
+
+``````````````````````````````````````````````````````````````````````````
+OUTPUT:
+Character at position 1 : e
+After concatenation: Hello Java
+
