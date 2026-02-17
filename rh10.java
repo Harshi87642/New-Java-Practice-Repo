0f13fe3fc1abf291be822273e615aa744cfb9bdc
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-17 23:36:48 +0530

    Simple practice program 10

---
+++ rh10.java
@@ -0,0 +1,22 @@
+// String operations
+
+public class rh10
+{
+  public static void main(String[] args)
+  {
+ 
+      String str = "Hello";
+     // String str1 = "java";
+  
+      System.out.println(str + "hi");
+     // System.out.println(str + str1);
+  }
+}
+
+
+
+
+`````````````````````````````````````````````````````````
+OUTPUT:
+Hello hi
+
