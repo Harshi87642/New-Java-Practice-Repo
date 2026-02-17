f91fef0bb45054d71a476e64c080cf0a89ca4a28
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-17 23:35:35 +0530

    Simple practice program 8

---
+++ rh08.java
@@ -0,0 +1,36 @@
+// Enhanced For loop
+
+public class rh08
+{
+  public static void main(String[] args)
+  {
+
+     int arr[] = new int[5];
+
+     arr[0]=1;
+      arr[1]=2;
+      arr[2]=3;
+       arr[3]=4;
+      arr[4]=5;
+
+      for(int a : arr)
+      {
+         System.out.println(a);
+       }
+   }
+}
+
+
+
+
+
+``````````````````````````````````````````````````````````````````````
+OUTPUT:
+1
+2
+3
+4
+5
+
+
+
