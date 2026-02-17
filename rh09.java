aa5e5b90f922c46d0432b421464848440355df37
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-17 23:36:25 +0530

    Simple practice program 9

---
+++ rh09.java
@@ -0,0 +1,24 @@
+// Strings in java
+
+
+public class rh09
+{
+  public static void main(String[] args)
+  {
+ 
+      String str = "Hello";
+  
+      System.out.println(str);
+  }
+}
+
+
+
+
+
+
+
+`````````````````````````````````````````````````````````````````
+
+OUTPUT:
+Hello
