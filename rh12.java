f09933facee675e60c8c04c506b83094622de5b8
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-19 22:37:01 +0530

    Simple practice program 12

---
+++ rh12.java
@@ -0,0 +1,38 @@
+// checking name email using String
+
+public class rh12
+{
+  public static void main(String[] args)
+   {
+   
+       String name = " Harshitha H R ";
+       String Password = "Pass@123";
+
+       // trim()->clean name
+      name.trim();
+      System.out.println("Clean Nmae: "+name);
+ 
+       if(password.length()>=8)
+       {
+         System.out.println("Valid Password");
+        }
+        else 
+        {
+          System.out.println("Invalid Password");
+         }
+    }
+}
+
+
+
+
+
+
+
+
+````````````````````````````````````````````````````````````````````````````````````````````````````````
+OUTPUT:
+Clean Nmae: Harshitha H R
+Valid Password
+
+
