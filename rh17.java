9679b64f9a8815be66d4e70d8bead8c4b3c9954c
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-19 22:43:06 +0530

    Simple practice program 17

---
+++ rh17.java
@@ -0,0 +1,23 @@
+// append using string builders
+
+public class rh17
+{
+  public static void main(String[] args)
+  {
+     String name = "Harshitha";
+    StringBuilder sb = new StringBuilder();
+
+    sb.append("welcome");
+     sb.append(name);
+     sb.append(!!!!!!");
+
+     System.out.println(sb);
+  }
+}
+
+
+
+
+```````````````````````````````````````````````````````````````````````````````
+OUTPUT:
+welcome Harshitha !!!!!!
