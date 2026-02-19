e20eec193c664a7f09f2de96964976166a365485
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-19 22:41:25 +0530

    Simple practice program 16

---
+++ rh16.java
@@ -0,0 +1,32 @@
+// Checking if the file is pdf or not and printing valid if it is pdf
+
+public class rh16
+{
+  public static void main(String[] args)
+  {
+   
+    String file = "Resume.pdf";
+ 
+    //using endswith()
+ 
+     if(file.endsWith(".pdf");
+     {
+        System.out.println("Valid resume");
+      }
+       else
+       {
+          System.out.println("Invalid resume");
+        }
+     
+   }
+}
+
+
+
+
+
+
+
+```````````````````````````````````````````````````````````````````
+OUTPUT:
+Valid resume
