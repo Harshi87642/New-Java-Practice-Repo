7fe6e2bffb608cab9fb61c3b1a112e64eebc9e84
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-22 23:44:51 +0530

    Simple practice program 25

---
+++ rh25.java
@@ -0,0 +1,22 @@
+// Constructor
+
+class Human
+{
+  
+      public Human()
+  {
+     System.out.println("hi");
+ }
+}
+
+ public class rh25
+{
+   public static void main(String[] args)
+   {
+         Human obj = new Human();
+         Human obj1 = new Human();
+    }
+}
+           
+  
+        
