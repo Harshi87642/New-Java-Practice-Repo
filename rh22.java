31ffd2f137985cc6177f2db4ded87aabcab5b570
Harshitha H R <harshiharshi87642@gmail.com>
2026-02-22 23:42:23 +0530

    Simple practice program 22

---
+++ rh22.java
@@ -0,0 +1,35 @@
+// Encapsulation
+
+class Human
+{
+   int age;
+    String name;
+}
+public class rh22
+{
+   public static void main(String[] args)
+    {
+
+      Human h = new Human();
+   
+       h.age=2;
+       h.name="Harsh";
+     
+      System.out.println(h.name);
+      System.out.println(h.age);
+    }
+}
+
+
+
+
+
+
+//```````````````````````````````````````````````````````
+     
+//O|P:
+//Harsh
+//2
+
+
+
