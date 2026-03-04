6eea460986aa2917bb357ffb56ee11161271c834
Harshitha H R <harshiharshi87642@gmail.com>
2026-03-04 20:20:59 +0530

    Simple practice program 45

---
+++ rh45.java
@@ -0,0 +1,34 @@
+// printing all the elements which are present in the ArrayList
+
+import java.util.ArrayList;
+
+public class rh45
+{
+  public static void main(String[] args)
+  {
+
+      ArrayList<Integer> list = new ArrayList<>();
+
+      list.add(1);
+       list.add(2);
+      list.add(3);
+      list.add(4);
+      list.add(5);
+      list.add(6);
+      list.add(7);
+      list.add(8);
+      list.add(9);
+      list.add(10);
+      list.add(11);
+
+     System.out.println(list);
+  }
+}
+
+
+
+
+
+
+//OUTPUT:
+//[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]
