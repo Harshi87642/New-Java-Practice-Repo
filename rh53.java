bc23eddda0fe38dc6e78badf07abf5bf9bc897d4
Harshitha H R <harshiharshi87642@gmail.com>
2026-03-06 23:40:29 +0530

    Simple practice program 53

---
+++ rh53.java
@@ -0,0 +1,39 @@
+// Finding the index of the number
+
+import java.util.ArrayList;
+
+public class rh53
+{
+  public static void main(String[] args)
+  {
+  
+     ArrayList<Integer> list = new ArrayList<>();
+ 
+     list.add(10);
+     list.add(20);
+     list.add(30);
+     list.add(40);
+     list.add(50);
+     list.add(60);
+     list.add(70);
+     list.add(80);
+     list.add(90);
+     list.add(100);
+
+   
+
+ 
+    System.out.println("The index of the 100 is: " + list.indexOf(100));
+
+  }
+}
+
+
+
+
+
+
+
+
+//OUTPUT:
+//The index of the 100 is: 9
