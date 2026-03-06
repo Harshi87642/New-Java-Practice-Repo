af289b9f0271b03510b3eb1a9448229eb1cd3ec0
Harshitha H R <harshiharshi87642@gmail.com>
2026-03-06 23:42:14 +0530

    Simple practice program 55

---
+++ rh55.java
@@ -0,0 +1,33 @@
+// Finding frequency of number
+
+  int count = 0;
+
+ for(int num : list)
+ {
+   if(num==12)
+   {import java.util.ArrayList;
+import java.util.Arrays;
+
+public class rh55
+{
+ public static void main(String[] args)
+  {
+
+   ArrayList<Integer> list = new ArrayList<>(Arrays.asList(10,11,12,12,12,12,12,13,14,145,16,17,18,19,12,12,13,12));
+
+
+     count++;
+   }
+
+ }
+System.out.println("the frequency of the 12 is:" +count);
+
+}
+}
+
+
+
+
+//OUTPUT:
+//the frequency of the 12 is:8
+
