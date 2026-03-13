bd926bc6cd306be4e9319a70e9f021e9af864b82
Harshitha H R <harshiharshi87642@gmail.com>
2026-03-13 23:38:07 +0530

    simple practice program 64

---
+++ rh64.java
@@ -0,0 +1,16 @@
+// Final Keyword
+
+public class rh64
+{
+  public static void main(String[] args)
+  {
+   final double PI = 3.14;
+
+   double radius = 5;
+
+    double area = PI* radius*radius;
+
+   System.out.println("Radius : "+radius);
+   System.out.println("Area : "+area);
+  }
+}
